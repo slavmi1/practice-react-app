8a59ebbda551e65acddb18cab6c07d1315a7c720
slavmi1 <subbotlln@mail.ru>
2026-07-04 17:24:01 +0500

    add a universal Select component

---
+++ src/components/ui/Select/Select.tsx
@@ -0,0 +1,46 @@
+import styles from "./Select.module.scss";
+
+import type { ComponentProps } from "react";
+
+type SelectOption = {
+  value: string;
+  label: string;
+};
+
+type SelectProps = ComponentProps<"select"> & {
+  label?: string;
+  error?: string;
+  options: SelectOption[];
+};
+
+const Select = (props: SelectProps) => {
+  const { label, error, options, id, ...selectProps } = props;
+
+  const selectClassName = error
+    ? `${styles.select__input} ${styles.select__input_error}`
+    : styles.select__input;
+
+  return (
+    <div className={styles.select}>
+      {label && (
+        <label className={styles.select__label} htmlFor={id}>
+          {label}
+        </label>
+      )}
+      <select {...selectProps} className={selectClassName} id={id}>
+        {options.map((option) => (
+          <option
+            key={option.value}
+            value={option.value}
+            className={styles.select__option}
+          >
+            {option.label}
+          </option>
+        ))}
+      </select>
+      {error && <span className={styles.select__error}>{error}</span>}
+    </div>
+  );
+};
+
+export default Select;
