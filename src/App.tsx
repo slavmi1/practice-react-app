4eb81e59ddc1c9ada995e4f21ab443a6e3d8a07c
slavmi1 <subbotlln@mail.ru>
2026-07-06 18:34:10 +0500

    add ServiceCard component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import Card from "./components/ui/Card/Card";
 import Field from "./components/ui/Field/Field";
 import Select from "./components/ui/Select/Select";
 import Badge from "./components/ui/Badge/Badge";
+import ServiceCard from "./components/ui/ServiceCard/ServiceCard";
+
+import mansHaircutIcon from "./assets/service_icons/mansHaircut.png";
+import beardDecorIcon from "./assets/service_icons/beardDecor.png";
+import hairAndBeardIcon from "./assets/service_icons/hairAndBeard.png";
+import kidsHaircutIcon from "./assets/service_icons/kidsHaircut.png";
+import hairDryerIcon from "./assets/service_icons/hairDryer.png";
 
 const App = () => {
   const services = [
@@ -21,6 +28,13 @@ const App = () => {
       }}
     >
       <h1>Запись в барбершоп</h1>
+      <ServiceCard
+        icon={mansHaircutIcon}
+        title="Мужская стрижка"
+        description="Классическая или современная стрижка"
+        duration={45}
+        price={1200}
+      />
       <Button type="submit">Добавить цель</Button>
       <Field
         type="select "
---
+++ src/components/ui/ServiceCard/ServiceCard.tsx
@@ -0,0 +1,44 @@
+import Button from "../Button/Button";
+import Card from "../Card/Card";
+
+import styles from "./ServiceCard.module.scss";
+
+import clockIcon from "../../../assets/service_icons/clock.svg";
+
+type ServiceCardProps = {
+  icon: string;
+  title: string;
+  description: string;
+  duration: number;
+  price: number;
+};
+
+const ServiceCard = (props: ServiceCardProps) => {
+  const { icon, title, description, duration, price } = props;
+
+  return (
+    <Card className={styles.serviceCard}>
+      <div className={styles.top}>
+        <img className={styles.icon} src={icon} alt="" aria-hidden="true" />
+
+        <div className={styles.content}>
+          <h3 className={styles.title}>{title}</h3>
+          <p className={styles.description}>{description}</p>
+        </div>
+      </div>
+
+      <hr className={styles.divider} />
+
+      <div className={styles.bottom}>
+        <span className={styles.duration}>
+          <img src={clockIcon} alt="" aria-hidden="true" />
+          {duration} мин
+        </span>
+        <span className={styles.price}>{price} ₽</span>
+        <Button className={styles.button}>Выбрать</Button>
+      </div>
+    </Card>
+  );
+};
+
+export default ServiceCard;
